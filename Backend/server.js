a85766e7014fde1b5093f052a3b1d13e3491103f
perumallaharshitha <harshithaperumalla23@gmail.com>
2025-03-10 18:32:44 +0530

    removed / in link

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(cors({
   // origin:'http://localhost:5173'
-  origin:['http://localhost:5173','https://campus-navigation-nu.vercel.app/']
+  origin:['http://localhost:5173','https://campus-navigation-nu.vercel.app']
 }))
 
 app.options("*", cors());
